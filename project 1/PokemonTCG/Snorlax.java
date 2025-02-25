bf2c0e2ef093b7da12c8a06e8f9d6c71c8bf1e51
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-02-25 13:31:30 -0500

    Update Snorlax.java

--- project 1/PokemonTCG/Snorlax.java
+++ project 1/PokemonTCG/Snorlax.java
@@ -1,11 +1,18 @@
-public class Snorlax extends Pokemon {
-    public Snorlax() {
-        super("Snorlax", 70, 15, 10, 10, 10, 2, new String[]{"Body Slam", "Rest"});
+// This class represents the Bulbasaur Pokemon card
+// It extends the abstract Pokemon class as all the pokemon will and provides its own implementation of the attack method
+
+public class Bulbasaur extends Pokemon {
+    // Constructor for the pokemon Bulbasaur
+    // Sets Bulbasaur's attributes 
+    public Bulbasaur() {
+        super("Bulbasaur", 45, 8, 7, 12, 10, 1, new String[]{"Vine Whip", "Tackle"});
     }
     
+    // Implementation of Bulbasaurs attack method
+    // Uses its first move Vine Whip to attack the opponent
     @Override
     public void attack(Pokemon opponent) {
         System.out.println(getName() + " uses " + moves[0] + "!");
-        opponent.hp -= attack;
+        opponent.hp -= specialAttack; // Inflict damage using special attack
     }
 }
