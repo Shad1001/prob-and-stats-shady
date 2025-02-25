8c52ba1b3d33e00076df155ad110e665a86b961f
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-02-25 13:30:38 -0500

    Update Card.java

--- project 1/PokemonTCG/Card.java
+++ project 1/PokemonTCG/Card.java
@@ -1,3 +1,6 @@
+//This is my class for all the Pokemon cards I in my TCG Game.
+//Every type of card revolves around it for examples my Pokemon, Energy Trainer 
+//It will extend from this class 
 public abstract class Card {
     protected String name;
     
