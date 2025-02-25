11394d8c5d6d9f4ff662aa799c1c4d911c0ca07e
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-02-25 13:31:47 -0500

    Update TrainerCard.java

--- project 1/PokemonTCG/TrainerCard.java
+++ project 1/PokemonTCG/TrainerCard.java
@@ -1,11 +1,17 @@
+// This class represents a Trainer card in the game
+// Trainer cards provide special effects (for example, drawing extra cards).
+// It extends the base Card class
 public class TrainerCard extends Card {
+    // Constructor for TrainerCard
     public TrainerCard(String name) {
         super(name);
     }
     
-    // Example trainer effect: draw a card for the player.
+    // Example method that simulates the trainer card's effect
+    // In this example, the effect is to allow the player to draw a card
+    // There will be different effects 
     public void useEffect(Player player) {
         System.out.println(getName() + " trainer card effect activated for " + player.getName() + "!");
-        player.drawCard();
+        player.drawCard(); // Call the player's drawCard() method to draw one extra card
     }
 }
