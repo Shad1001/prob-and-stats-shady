f9341c9255bf606c5315baafdfd7fd042cec65a5
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-02-25 13:31:16 -0500

    Update EnergyCard.java

--- project 1/PokemonTCG/EnergyCard.java
+++ project 1/PokemonTCG/EnergyCard.java
@@ -1,11 +1,18 @@
+// This class represents my Energy cards in the game
+// Energy cards are used to power Pokemon attacks and other actions
+// It extends the base Card class
 public class EnergyCard extends Card {
+    // The type of energy this card represents
     private String energyType;
     
+    // Constructor for EnergyCard
+    // It appends "Energy" to the energyType to form the card's name
     public EnergyCard(String energyType) {
         super(energyType + " Energy");
         this.energyType = energyType;
     }
     
+    // Getter method to retrieve the energy type
     public String getEnergyType() {
         return energyType;
     }
