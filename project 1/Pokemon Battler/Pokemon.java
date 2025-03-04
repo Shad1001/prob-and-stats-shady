f70188aadedf2885ee67f2f67d7f19fe2dfb0f41
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-03-03 19:46:39 -0500

    Update Pokemon.java

--- project 1/Pokemon Battler/Pokemon.java
+++ project 1/Pokemon Battler/Pokemon.java
@@ -1,1 +1,41 @@
+public class Pokemon {
+    // Global variables
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
 
+    // Getters
+    public int getHp() {
+        return hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    // Setters
+    public void setHp(int userHp) {
+        hp = Math.max(userHp, 0); // Ensure HP is never negative
+    }
+
+    public void setAttack(int userAttack) {
+        this.attack = userAttack;
+    }
+
+    public void setDefense(int userDefense) {
+        defense = userDefense;
+    }
+
+    public void setSpeed(int userSpeed) {
+        speed = userSpeed;
+    }
+}
