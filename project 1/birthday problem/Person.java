e5ca0d95e000b8fd4a1eeae3c9c1421a8b796567
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-03-04 15:30:20 -0500

    Update Person.java

--- project 1/birthday problem/Person.java
+++ project 1/birthday problem/Person.java
@@ -1,1 +1,20 @@
+import java.util.Random;
 
+public class Person {
+    private int dayOfBirth;
+    Random rng = new Random();
+
+    public Person() {
+        dayOfBirth = rng.nextInt(365);
+    }
+
+    // Returns the Person's birthday (a day between 0 and 364)
+    public int fetchBirthday() {
+        return dayOfBirth;
+    }
+
+    // Sets the Person's birthday
+    public void changeBirthday(int newBirthDay) {
+        dayOfBirth = newBirthDay;
+    }
+}
