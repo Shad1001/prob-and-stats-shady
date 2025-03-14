be9dddc0a8b6438d12455e30a089e570b221c70f
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-03-14 15:20:20 -0400

    Delete project 1/Pokemon Battler/public class Pikachu extend Pokemon.java

--- project 1/Pokemon Battler/public class Pikachu extend Pokemon.java
+++
@@ -1,9 +0,0 @@
-public class Pikachu extends Pokemon {
-    // Default Pikachu stats
-    public Pikachu() {
-        setHp(35);
-        setAttack(55);
-        setDefense(30);
-        setSpeed(90);
-    }
-}
