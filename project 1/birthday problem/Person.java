5287aa6cbb8f2d397b134cc02fde49d26fc3712b
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-03-15 15:39:00 -0400

    Update Person.java

--- project 1/birthday problem/Person.java
+++ project 1/birthday problem/Person.java
@@ -1,19 +1,37 @@
 import java.util.Random;
 
+/**
+ * The Person class represents an individual with a randomly assigned birthday.
+ * Each Person's birthday is represented as an integer corresponding to a day of the year.
+ */
 public class Person {
+    // The day of the year representing the birthday 
     private int dayOfBirth;
+    // Random number generator for assigning a birthday.
     Random rng = new Random();
 
+    /**
+     * Constructor for Person.
+     * It initializes the birthday to a random day between 0 and 364.
+     */
     public Person() {
-        dayOfBirth = rng.nextInt(365);
+        dayOfBirth = rng.nextInt(365); // Generates a random integer between 0 and 365 
     }
 
-    // Returns the Person's birthday (a day between 0 and 364)
+    /**
+     * Retrieves the birthday of the person.
+     *
+     * @return An integer representing the person's birthday.
+     */
     public int fetchBirthday() {
         return dayOfBirth;
     }
 
-    // Sets the Person's birthday
+    /**
+     * Allows changing the person's birthday.
+     *
+     * @param newBirthDay The new birthday value.
+     */
     public void changeBirthday(int newBirthDay) {
         dayOfBirth = newBirthDay;
     }
