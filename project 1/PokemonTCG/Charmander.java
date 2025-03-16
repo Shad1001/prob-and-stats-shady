baaa72b246272b6c53233a8c45829dd71a46f62e
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-03-16 16:50:35 -0400

    Update Charmander.java

--- project 1/PokemonTCG/Charmander.java
+++ project 1/PokemonTCG/Charmander.java
@@ -1,17 +1,66 @@
-// This class represents the Charmander Pokemon card
-// It extends the abstract Pokemon class as I said previously in my pokemon.java file and provides its own implementation of the attack method
+/**
+ * Charmander.java
+ * Charmander TCG card.
+ * "Collect": 0 damage, 0 energy cost (effect: draw a card).
+ * "Flare": 30 damage, requires 1 Fire Energy.
+ */
 public class Charmander extends Pokemon {
-    // Constructor for Charmander
-    // Initializes Charmander with its specific attributes 
+
+    // Constructor: Initializes the Charmander card with its attributes and available attacks.
     public Charmander() {
-        super("Charmander", 50, 10, 5, 15, 5, 1, new String[]{"Ember", "Scratch"});
+        // Call the superclass (Pokemon) constructor with the following parameters:
+        // Name: "Charmander"
+        // HP: 70
+        // Type: "Fire"
+        // Retreat Cost: 1
+        // Attacks: an array containing two Attack objects:
+        //  "Collect": 0 damage, no energy required (effect: draw a card)
+        //  "Flare": 30 damage, requires 1 Fire Energy
+        super("Charmander", 70, "Fire", "Water", 1, new Attack[] {
+            new Attack("Collect", 0, 0, ""),
+            new Attack("Flare", 30, 1, "Fire")
+        });
     }
     
-    // Implementation of the attack method for Charmander
-    // Charmander uses its first move which is Ember to attack subtracting its specialAttack value from the opponents HP
+    // Override the attack method to implement Charmander's specific attack actions.
     @Override
-    public void attack(Pokemon opponent) {
-        System.out.println(getName() + " uses " + moves[0] + "!");
-        opponent.hp -= specialAttack; // Inflict damage using special attack
+    public void attack(Player attackingPlayer, Pokemon opponent, int attackIndex) {
+        if (attackIndex < 0 || attackIndex >= attacks.length) {
+            System.out.println(getName() + " does not have that attack.");
+            return;
+        }
+        
+        Attack chosenAttack = attacks[attackIndex];
+        String moveName = chosenAttack.getName();
+        
+        // Check if the attack requires energy and if Charmander has sufficient energy to perform it.
+        if (chosenAttack.getEnergyCost() > 0 && !hasEnergyForAttack(chosenAttack)) {
+            System.out.println(getName() + " does not have enough energy to use " + moveName + "!");
+            return;
+        }
+        
+        // Say the attack being used.
+        System.out.println(getName() + " uses " + moveName + "!");
+        
+        // Determine which attack to execute based on the move name.
+        if (moveName.equalsIgnoreCase("Collect")) {
+            // "Collect" attack: allows the player to draw a card.
+            System.out.println(getName() + " activates Collect and draws a card!");
+            attackingPlayer.drawCard();  // Player draws a card.
+        } else if (moveName.equalsIgnoreCase("Flare")) {
+            // "Flare" attack: deals damage to the opponent.
+            int damage = chosenAttack.getDamage();
+            
+            // Check if the opponent's weakness matches Charmander's type ("Fire").
+            // If so, double the damage dealt.
+            if (opponent.weakness.equalsIgnoreCase(this.type)) {
+                damage *= 2;
+                System.out.println("It's super effective! Damage is doubled.");
+            }
+            
+            // Apply the damage to the opponent's HP.
+            opponent.hp -= damage;
+            System.out.println(opponent.getName() + " takes " + damage + " damage. Remaining HP: " + opponent.hp);
+        }
     }
 }
