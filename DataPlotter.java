fd9199386d428398cf7d55e029101d70eab736e4
Shad1001 <159466461+Shad1001@users.noreply.github.com>
2025-05-01 14:50:18 -0400

    Delete DataPlotter.java

--- DataPlotter.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class DataPlotter {
-
-    public static void exportData(List<DataHandler.Point> data) {
-        // Export the data using DataHandler's export method.
-        DataHandler.exportCSV(data, "exported_data.csv");
-    }
-}
